d2899ecd6d08b10e43d19ecfb954ef1d859f045f
백준하 <astoru@naver.com>
2020-07-16 22:09:38 +0900

    array 1756 더 낮은 시간 복잡도를 적용하기 위해 전처리 과정이 필요할 수도 있다는 idea

--- BaekjoonOJ/Array/acmicpc1756.py
+++ BaekjoonOJ/Array/acmicpc1756.py
@@ -2,26 +2,36 @@ oven_depth, dough_counts = map(int, input().split())
 oven_radius_list = list(map(int, input().split()))
 dough_radius_list = list(map(int, input().split()))
 
-cur_depth = oven_depth
-cannot_be_added = False
+oven_index = oven_depth - 1
 
-for cur_radius in dough_radius_list:
-    if cur_depth == 0:
-        cannot_be_added = True
-        break
-    for i in range(0, cur_depth):
-        if cur_radius > oven_radius_list[i]:
-            cur_depth = i
-            # print(i, end=",")
-            # print(cur_radius)
-            break
-        elif i == cur_depth-1:
-            # print(i, end=",")
-            # print(cur_radius)
-            cur_depth = cur_depth-1
+# 평탄화 지름 = 맨 처음 지름
+flattening_radius = oven_radius_list[0]
 
+for non_flattened_oven_index in range(oven_depth):
+    # 현재 지름이 평탄화 지름보다 크거나 같다면
+    if oven_radius_list[non_flattened_oven_index] >= flattening_radius:
+        # 현재지름 = 평탄화지름
+        oven_radius_list[non_flattened_oven_index] = flattening_radius
+    else:
+        # 아니라면 평탄화지름 = 현재지름
+        flattening_radius = oven_radius_list[non_flattened_oven_index]
 
-if cannot_be_added:
+# 평탄화 결과 체크
+# for i in range(oven_depth):
+#     print(oven_radius_list[i])
+
+dough_index = 0
+
+for flattened_oven_index in reversed(range(oven_depth)):
+    # 현재 오븐에 도우가 들어간다면
+    if oven_radius_list[flattened_oven_index] >= dough_radius_list[dough_index]:
+        # 다음 도우로
+        dough_index = dough_index + 1
+        # 그게 마지막도우였다면
+        if dough_index == dough_counts:
+            print(flattened_oven_index + 1)
+            exit(0)
+
+# 오븐 끝에 도달했을 경우
+if flattened_oven_index == 0:
     print(0)
-else:
-    print(cur_depth)
